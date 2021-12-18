f4c0d2a5c6fe2822d44bb85119187aa042619d15
VladimirChb <96146610+VladimirChb@users.noreply.github.com>
2021-12-18 21:56:17 +0300

    внесены начальные изменения

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Main from "./pages/Main";
+import Tour from "./pages/Tour";
+import Private from "./pages/Private";
+import {createContext, useEffect, useState} from "react";
+import {cards} from "./database";
+import Send from "./pages/Send";
+
+export const AppContext = createContext(null)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tours, setTours] = useState([])
+    const [isAuth, setIsAuth] = useState(false)
+
+    useEffect(() => {
+        const response = cards
+        setTours(response)
+    }, [])
+
+    return (
+        <AppContext.Provider value={{tours, setTours, isAuth, setIsAuth}}>
+            <Navigation/>
+
+            <main style={{padding: 32}}>
+                <Routes>
+                    <Route path="/" element={<Main/>}/>
+                    <Route path="/tour/:id" element={<Tour/>}/>
+                    <Route path="/private" element={<Private/>}/>
+                    <Route path="/send/:tourId" element={<Send/>}/>
+                    <Route path="*" element={<Navigate to='/'/>}/>
+                </Routes>
+            </main>
+
+        </AppContext.Provider>
+    );
 }
 
 export default App;
